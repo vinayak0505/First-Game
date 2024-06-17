45345765c4862c72d936d99276cc72fa64f7f299
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-06-17 23:03:35 +0530

    completed level 4

---
+++ scripts/bear_trap.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var catch_timer = $CatchTimer
+@onready var reset_timer = $ResetTimer
+@onready var collision_shape_2d = $KillZone/CollisionShape2D
+
+#for checking if the animation has triggered or not
+var started = false;
+
+func _on_body_entered(body):
+	if(started): 
+		return;
+	started = true;
+	animated_sprite_2d.play("ready");
+	catch_timer.start(1);
+
+@onready var audio_stream_player = $AudioStreamPlayer
+
+func _on_timer_timeout():
+	print("timeout")
+	reset_timer.start(1);
+	animated_sprite_2d.play("catch");
+	collision_shape_2d.set_disabled(false);
+	audio_stream_player.play();
+
+func _on_reset_timer_timeout():
+	print("reset")
+	collision_shape_2d.set_disabled(true);
+	animated_sprite_2d.play("idle");
+	started = false;
--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const SPEED = 60;
+@export var SPEED : int = 60;
 var direction = 1;
 
 @onready var ray_cast_right = $RayCastRight
