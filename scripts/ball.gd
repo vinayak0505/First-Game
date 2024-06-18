11be33e79d0c42404e3588635e5509bf0ca404d5
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-06-18 15:36:37 +0530

    created a bouncing ball

---
+++ scripts/ball.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var _velocity = 0;
+var bounce = 0.9;
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if is_on_floor():
+		var angle = get_floor_angle();
+		_velocity = -_velocity * bounce * (1 - angle);
+		velocity.x += _velocity * angle;
+		velocity.x *= bounce;
+
+	_velocity += gravity * delta;
+	velocity.y = _velocity;
+	
+	move_and_slide()
--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -3,10 +3,11 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
+	#if(body.name == "Player"):
 	print("You Died!")
 	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
+	body.get_node("CollisionShape2D").queue_free()
 
 func _on_timer_timeout():
 	Engine.time_scale = 1.0;
