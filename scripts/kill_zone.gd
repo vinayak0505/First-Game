79ab2e0ae22a4f10b52a2f6b07ed91c9f45636f0
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-06-22 18:50:26 +0530

    fixed level 4

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -7,7 +7,9 @@ func _on_body_entered(body):
 		print("You Died!")
 		Engine.time_scale = 0.5
 		timer.start()
-	body.get_node("CollisionShape2D").queue_free()
+		body.get_node("CollisionShape2D").queue_free()
+	else:
+		body.queue_free();
 
 func _on_timer_timeout():
 	Engine.time_scale = 1.0;
