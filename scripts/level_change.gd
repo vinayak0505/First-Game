2a913cb8290102af8204fc28d71ff375d4a759ca
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-06-23 19:42:54 +0530

    create select level page

--- scripts/level_change.gd
+++ scripts/level_change.gd
@@ -4,10 +4,17 @@ var save_path = "user://variables.save";
 
 const PREFIX = 'res://scenes/level_';
 const POSTFIX = '.tscn';
+const LEVEL_SELECT = "res://scenes/level_select.tscn";
 
-var levelScene = null;
-var coins = 0;
-var current_level = 1;
+@onready var levelScene = null;
+
+var coins = [];
+var unlock_level = 1;
+var current_level = null;
+
+#@onready var player_point_label = %PlayerPointLabel
+@onready var coins_count_label = $"../CanvasLayer/HBoxContainer/MarginContainer/CoinsCount"
+@onready var level_label = $"../CanvasLayer/HBoxContainer/MarginContainer/Level"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,36 +22,48 @@ func _ready():
 	loadData();
 	loadLevel();
 
-func changeChild():
+func changeChild(level=null):
 	unloadLevel();
-	if(current_level == null):
-		current_level = 0;
-	current_level += 1;
+	if (level == null):
+		current_level += 1;
+	else:
+		current_level = level;
+
+	if (current_level > unlock_level):
+			unlock_level = current_level
 	loadLevel();
 	saveData();
 
 func unloadLevel():
-	if (levelScene):
-		levelScene.queue_free();
+	for child in get_children():
+		child.queue_free();
 
 func loadLevel():
-	level.text = "Level: " + str(current_level);	
-	var newLevel = load(PREFIX + str(current_level) + POSTFIX)
-	levelScene = newLevel.instantiate()
-	call_deferred("add_child", levelScene)
-
-#@onready var player_point_label = %PlayerPointLabel
-@onready var coins_count = $"../CanvasLayer/HBoxContainer/CoinsCount"
-@onready var level = $"../CanvasLayer/HBoxContainer/Level"
+	if(current_level != null):
+		level_label.text = "Level: " + str(current_level);
+		var newLevel = load(PREFIX + str(current_level) + POSTFIX)
+		levelScene = newLevel.instantiate()
+		add_child(levelScene);
+		print(coins)
+		print(current_level - 1);
+		coins[current_level - 1] = 0;
+		coins_count_label.text = "Coins: 0" ;
+		level_label.text = "Level: " + str(current_level);
+	else:
+		var newLevel = load(LEVEL_SELECT)
+		levelScene = newLevel.instantiate()
+		coins_count_label.text = "Total Coins: " + str(_total_coins());
+		level_label.text = "";
+		add_child(levelScene);
 
 func add_point():
-	coins += 1
-	coins_count.text = "Coins: " + str(coins);
+	coins[current_level - 1] += 1;
+	coins_count_label.text = "Coins: " + str(coins[current_level - 1]);
 
 func saveData():
 	var file = FileAccess.open(save_path, FileAccess.WRITE);
 	file.store_var({
-		"current_level": current_level,
+		"unlock_level": unlock_level,
 		"coins": coins
 	});
 	file.close();
@@ -53,12 +72,19 @@ func loadData():
 	if (FileAccess.file_exists(save_path)):
 		var file = FileAccess.open(save_path, FileAccess.READ);
 		var data = file.get_var();
-		unPack(data);
 		file.close()
+		unPack(data);
+	else:
+		unPack({});
 
 func unPack(data):
-	current_level = data["current_level"]
-	coins = data["coins"]
-	coins_count.text = "Coins: " + str(coins);
-	level.text = "Level: " + str(current_level);
-	return
+	unlock_level = data.get('unlock_level', 1);
+	print(unlock_level);
+	if 'coins' in data and data['coins'] is Array and data['coins'].size() == 11:
+		coins = data["coins"]
+	else:
+		coins.resize(11);
+		coins.fill(0);
+
+func _total_coins():
+	return coins.reduce(func(a, b): return a + b);
---
+++ scripts/level_select_boss.gd
@@ -0,0 +1,15 @@
+extends Button
+
+@onready var level_change = $"../../../../..";
+var level;
+
+func _ready():
+	level = name.replace("Button", "").to_int();
+	if (level_change.unlock_level >= level):
+		disabled = false;
+	else:
+		disabled = true;
+
+func _pressed():
+	if (disabled): return;
+	level_change.changeChild(name.to_int());
